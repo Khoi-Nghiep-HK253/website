9667d5eb21ad548e9c904548cfc243e0d0ced408
Đoàn Trí Hùng <koikoidth12@gmail.com>
2026-08-07 01:34:34 +0700

    feat: add Storybook documentation and interactive demo for CustomTabPanel component

--- src/components/CustomTabPanel/CustomTabPanel.stories.tsx
+++ src/components/CustomTabPanel/CustomTabPanel.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
